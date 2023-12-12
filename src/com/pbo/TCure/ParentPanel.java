989721496edc86ee148be6795ee01d75743afb94
Inventiner <46766058+Inventiner@users.noreply.github.com>
2023-12-12 16:16:25 +0700

    fix keylistener

--- src/com/pbo/TCure/ParentPanel.java
+++ src/com/pbo/TCure/ParentPanel.java
@@ -2,24 +2,34 @@ package com.pbo.TCure;
 
 import java.awt.CardLayout;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class ParentPanel extends JPanel{
+public class ParentPanel extends JPanel implements KeyListener{
 	private CardLayout cardlayout = new CardLayout();
+	static GamePanel game;
 	
 	public ParentPanel(JFrame masterFrame) {
 		setLayout(cardlayout);
+		addKeyListener(this);
+		game = new GamePanel(masterFrame);
+		
 		
 		// Panel game
 		add(new MainMenuPanel(),"MainMenu");
-		add(new GamePanel(masterFrame), "GameScreen");
+		add(game, "GameScreen");
 		add(new WinPanel(), "Win");
 	}
 	
 	public void showPanel(String name) {
 		cardlayout.show(this, name);
+		
+		if(name == "GameScreen") {
+			game.requestFocus();
+		}
 	}
 	
 	public void draw(Graphics g) {
@@ -29,5 +39,23 @@ public class ParentPanel extends JPanel{
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		draw(g);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
